181d4180fb9d4634b31e70136a6b40593d6e78e2
Chi Mei Wang <kimmy0929@gmail.com>
2020-03-08 03:56:28 +0900

    create dropdown

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import Tooltip from './scripts/tooltip';
 import './styles/main.scss';
+import Tooltip from './scripts/tooltip';
+import Dropdown from './scripts/dropdown';
 
 //create a tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
 tooltip.init();
+
+//create dropdown
+const dropdowns = document.querySelectorAll('.dropdown');
+dropdowns.forEach(dropdown => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+});
--- src/scripts/tooltip.js
+++ src/scripts/tooltip.js
@@ -1,3 +1,5 @@
+import '../styles/tooltip.scss';
+
 class Tooltip {
   constructor(element) {
     this.element = element;
